227d21aae8c87477769854ef22b8594f3834a0f3
Sang <nqsang@fossil.com>
2020-03-14 18:53:16 +0700

    Clear some debug lines

--- matrix/src/linear_regression.c
+++ matrix/src/linear_regression.c
@@ -46,78 +46,16 @@ Vector *linear_regression(char *sample_input_file, char *sample_output_file)
 	Vector *weight        = mul_matrix(A_invert,b);
 	// display_vector(weight);
 
-	// free_mat(input);
-	// free_vector(output);
-	// free_mat(one_bar);
-	// free_mat(Xbar);
-	// free_mat(Xbar_t);
+	free_mat(input);
+	free_vector(output);
+	free_mat(one_bar);
+	free_mat(Xbar);
+	free_mat(Xbar_t);
 	// free_mat(A);
-	// free_mat(b);
-	// free_mat(A_invert);
+	free_mat(b);
+	free_mat(A_invert);
 
 	return weight;
-#if 0
-	free_mat(total_input);
-	free_mat(line);
-
-	Matrix *data_t = transpose_matrix(data);
-	Matrix *output_data_t = transpose_matrix(output_data);
-
-	Matrix *one_bar = ones(mat_getrow(data_t),VER);
-	Matrix *Xbar = concatenate_mat(one_bar,data_t,HOR);
-	free(one_bar);
-
-	Matrix *Xbar_t = transpose_matrix(Xbar);
-	Matrix *A = mul_matrix(Xbar_t,Xbar);
-	Matrix *b = mul_matrix(Xbar_t,output_data_t);
-
-	free_mat(Xbar_t);
-
-	free_mat(data_t);
-
-	Matrix *w = mul_matrix(inv_mat(A),b);
-
-	printf("A = \n");
-	display_matrix(A);
-	printf("b = \n");
-	display_matrix(b);
-
-	printf("w = inv(A).b =\n");
-	display_matrix(w);
-
-	File_mat *f = get_file("./mat_files/input.mat");
-	Matrix *i = init_matrix(f);
-	Matrix *input = transpose_matrix(i);
-	free_mat(i);
-	//Matrix *output_mat = transpose_matrix(mul_matrix(concatenate_mat(ones(input->row,VER),input,HOR),w));
-	Matrix *one = ones(input->row,VER);
-	Matrix *output_mat1 = concatenate_mat(one,input,HOR);
-	free_mat(one);
-	Matrix *output_mat2 = mul_matrix(output_mat1,w);
-	Matrix *output_mat = transpose_matrix(output_mat2);
-	free_mat(output_mat1);
-	free_mat(output_mat2);
-	printf("Output data:\n");
-	display_matrix(output_mat);
-	//display_matrix(transpose_matrix(mul_matrix(Xbar,w)));
-	//display_matrix(weight);
-
-
-	free_mat(output_data);
-	free_mat(data);
-	free_mat(A);
-	free(b);
-	free_mat(w);
-	free_mat(output_mat);
-	free_mat(output_data_t);
-	free_mat(input);
-	free_mat(Xbar);
-	free(data_info);
-	free(f);
-
-#endif
-
-	return 0;
 }
 
 Vector *apply_linear_rg(Vector *weight, char *file) {
